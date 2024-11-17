ecd080c2d2f9ccec8bc2a344b2ab3edfefb58d21
Melissa Andrade Garcia Da Silva <meandrad@c1r8p1.42sp.org.br>
2024-11-17 17:59:54 -0300

    get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,23 +6,22 @@
 /*   By: meandrad <meandrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 21:44:32 by meandrad          #+#    #+#             */
-/*   Updated: 2024/11/17 14:00:54 by meandrad         ###   ########.fr       */
+/*   Updated: 2024/11/17 16:25:14 by meandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 
-char	*read_line(int fd, int *bytes)
+char	*read_line(int fd)
 {
 	char	*t_buffer;
+	int		bytes_read;
 	
 	t_buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!t_buffer)
 		return (NULL);
-	*bytes = read(fd, t_buffer, BUFFER_SIZE);
-	if (*bytes < 0)
-		//break ;
+	bytes_read = read(fd, t_buffer, BUFFER_SIZE);
 }
 
 char	*get_next_line(int fd)
@@ -32,7 +31,5 @@ char	*get_next_line(int fd)
 	
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	bytes = 1;
-	line = read_line(fd, &bytes);
-	
+
 }
